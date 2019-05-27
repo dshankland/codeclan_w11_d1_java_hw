1ec8d3a2d86096a04db607a91a10381e84a3ca95
Darren Shankland <ds@4pq.co.uk>
2019-05-27 15:51:07 +0100

    add Calculator class

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+//    public Calculator() {
+//
+//    }
+
+    public int addValues(int firstInt, int secondInt) {
+        return firstInt + secondInt;
+    }
+
+    public int subtractValues(int firstInt, int secondInt) {
+        return firstInt - secondInt;
+    }
+
+    public int multiplyValues(int firstInt, int secondInt) {
+        return firstInt * secondInt;
+    }
+
+    public int divideValues(int firstInt, int secondInt) {
+        return firstInt / secondInt;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.addValues(2,3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(7, calculator.subtractValues(12,5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(15, calculator.multiplyValues(3,5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(5, calculator.divideValues(25,5));
+    }
+}
